76a2309a80a111afe6eba4a8570ad34ba44ca05c
zx <12345678>
2023-04-26 17:24:18 +0800

    add entity

---
+++ SherpaOnnx.Core/Model/OfflineTransducer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SherpaOnnx.Core.Model
+{
+    public class OfflineTransducer
+    {
+        public string EncoderFilename { get; set; }
+        public string DecoderFilename { get; set; }
+        public string JoinerFilename { get; set; }
+    }
+}
