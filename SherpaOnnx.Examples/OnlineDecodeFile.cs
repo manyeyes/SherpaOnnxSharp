9648b7907067868a271f1ce65879faf00c9df7cd
zx <12345678>
2023-04-28 18:16:25 +0800

    update method

--- SherpaOnnx.Examples/OnlineDecodeFile.cs
+++ SherpaOnnx.Examples/OnlineDecodeFile.cs
@@ -171,38 +171,6 @@ transducer Usage:
             }
             return argsDict;
         }
-
-        public void ssss(string encoder, string decoder,string joiner,string tokens,int numThreads,bool isDebug,string decodingMethod,List<string> wavFiles,ref TimeSpan total_duration)
-        {
-            OnlineTransducer onlineTransducer = new OnlineTransducer();
-            onlineTransducer.EncoderFilename = encoder;
-            onlineTransducer.DecoderFilename = decoder;
-            onlineTransducer.JoinerFilename = joiner;
-            //test online
-            OnlineRecognizer<OnlineTransducer> onlineRecognizer = new OnlineRecognizer<OnlineTransducer>(
-            onlineTransducer,
-            tokens,
-            num_threads: numThreads,
-            debug: isDebug,
-            decoding_method: decodingMethod);
-            List<float[]> samplesList = new List<float[]>();
-            foreach (string wavFile in wavFiles)
-            {
-                TimeSpan duration = TimeSpan.Zero;
-                float[] samples=Utils.AudioHelper.GetFileSamples(wavFile, ref duration);
-                samplesList.Add(samples);
-                total_duration += duration;
-            }
-            TimeSpan start_time = new TimeSpan(DateTime.Now.Ticks);
-            List<OnlineStream> streams = onlineRecognizer.CreateStreams(samplesList);
-            onlineRecognizer.DecodeMultipleStreams(streams);
-            List<OnlineRecognizerResultEntity> results = onlineRecognizer.GetResults(streams);
-            foreach (OnlineRecognizerResultEntity result in results)
-            {
-                Console.WriteLine(result.text);
-            }
-            TimeSpan end_time = new TimeSpan(DateTime.Now.Ticks);
-        }
         
     }
 }
